9f1a92fe0ea9fe4a28c32f87c13ab2c0c80bce5b
Graham Hay <grahamrhay@gmail.com>
2015-10-18 19:42:08 +0100

    Answer call

--- priv/static/js/call.js
+++ priv/static/js/call.js
@@ -26,5 +26,23 @@ VO_CALL = (function(socket) {
         });
     }
 
+    socket.on("initiate_call", function(msg) {
+        var pc = new RTCPeerConnection();
+        var onError = pc.close;
+        navigator.getUserMedia({video: true, audio: true}, function(stream) {
+            pc.addStream(stream);
+            var offer = JSON.parse(msg.offer);
+            pc.setRemoteDescription(new RTCSessionDescription(offer), function() {
+                pc.createAnswer(function(answer) {
+                    pc.setLocalDescription(new RTCSessionDescription(answer), function() {
+                        socket.send({type: 'answer', who: msg.from, answer: JSON.stringify(answer)});
+                    }, onError);
+                }, onError);
+            });
+        }, function(err) {
+            console.log("Error getting video stream: " + err);
+        });
+    });
+
     return module;
 })(VO_SOCKET);
